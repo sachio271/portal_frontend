999b8d5260b1ea39a20886948a2e465aab63e626
sachio271 <105487625+sachio271@users.noreply.github.com>
2024-09-09 16:51:19 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 
-module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 });
-
